dc9e588191f2a4722aca10416ad1e7dae707bf19
Muhammad Aliyul Murtadlo <muhammadaliyulm@gmail.com>
2020-01-14 12:59:12 +0700

    add search fitur

--- server/routes/api/fitur/fitur.controller.js
+++ server/routes/api/fitur/fitur.controller.js
@@ -2,8 +2,22 @@ let Fitur = require('./fitur.model')
 
 module.exports = {
   findAll: (req, res, next) => {
-    Fitur.find()
-      .then(fitur => res.json(fitur))
+    const { where, limit, offset, sort } = query(req.query)
+    const count = Fitur.countDocuments(where)
+    const search = req.query.search
+    if(search){
+      where['$or'] = [{
+        nama: {'$regex': search, '$options': 'i'}
+      }]
+    }
+    const data = Fitur.find(where).limit(limit).skip(offset).sort(sort).select('-__v')
+    Promise.all([count, data])
+      .then(cb=>{
+        res.json({
+            count: cb[0],
+            results: cb[1]
+        })
+      })
       .catch(error => next(error))
   },
   findById: (req, res, next) => {
